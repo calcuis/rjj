5d8d7b4773d8224cc776388513f8a8f7453baf33
calcuis <113646141+calcuis@users.noreply.github.com>
2024-08-19 09:53:45 +0800

    Update rjj.py

--- rjj.py
+++ rjj.py
@@ -1,9 +1,43 @@
 # !/usr/bin/env python3
 
-__version__="0.1.1"
+__version__="0.1.3"
 
-import argparse, os, json, csv
+import argparse, os, json, csv, glob
 import pandas as pd
+
+def filter():
+    keyword = input("Please provide a search keyword to perform this mass filter: ")
+    output_file = input("Please give a name to the output file: ")
+
+    if output_file != "":
+        output = f'{output_file}.csv'
+    else:
+        output = 'output.csv'
+
+    output_df = pd.DataFrame(columns=['Source_file', 'Column_y', 'Row_x'])
+
+    ask = input("Scan sub-folder(s) as well (Y/n)? ")
+    if  ask.lower() == 'y':
+        csv_files = [file for file in glob.glob('**/*.csv', recursive=True) if os.path.basename(file) != output]
+    else:
+        csv_files = [file for file in glob.glob('*.csv') if file != output]
+
+    for file in csv_files:
+        df = pd.read_csv(file)
+        for row_idx, row in df.iterrows():
+            for col_idx, value in row.items():
+                if isinstance(value, str) and keyword in value:
+                    print(f"Matched record found: {file}, Row: {row_idx + 1}, Column: {df.columns.get_loc(col_idx) + 1}, Value: {value}")
+                    new_row = {
+                        'Source_file': file,
+                        'Column_y': df.columns.get_loc(col_idx) + 1,
+                        'Row_x': row_idx + 1
+                    }
+                    combined_row = {**new_row, **row}
+                    output_df = output_df._append(combined_row, ignore_index=True)
+
+    output_df.to_csv(output, index=False)
+    print(f"Results of massive filtering saved to '{output}'")
 
 def convertor():
     json_files = [file for file in os.listdir() if file.endswith('.json')]
@@ -243,6 +277,7 @@ def __init__():
     subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", help="choose a subcommand:")
     subparsers.add_parser('c', help='convert json to csv')
     subparsers.add_parser('r', help='convert csv to json')
+    subparsers.add_parser('k', help='keyword filter for csv data')
     subparsers.add_parser('d', help='detect co-existing record(s)')
     subparsers.add_parser('j', help='joint all csv(s) together')
     subparsers.add_parser('s', help='split csv to piece(s)')
@@ -255,6 +290,8 @@ def __init__():
         jointer(output_file)
     elif args.subcommand == 's':
         spliter()
+    elif args.subcommand == 'k':
+        filter()
     elif args.subcommand == 'd':
         detector()
     elif args.subcommand == 'c':
