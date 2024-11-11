c341bd528b31392ab327f077617fee526471b459
calcuis <113646141+calcuis@users.noreply.github.com>
2024-11-11 08:07:11 +0800

    Update rjj.py

--- rjj.py
+++ rjj.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python3
 
-__version__="0.8.5"
+__version__="0.8.6"
 
-import argparse, os, json, csv, glob, hashlib, warnings, base64, random, math
+import argparse, io, os, json, csv, glob, hashlib, warnings, base64, random, math
 from sklearn.base import BaseEstimator, TransformerMixin
 from collections import defaultdict
 from datetime import datetime
@@ -3079,6 +3079,209 @@ def run_efa_fixed():
     else:
         print("No file selected.")
 
+def jsminifier(js, **kwargs):
+    klass = io.StringIO
+    ins = klass(js)
+    outs = klass()
+    JavascriptMinify(ins, outs, **kwargs).minify()
+    return outs.getvalue()
+
+class JavascriptMinify(object):
+    def __init__(self, instream=None, outstream=None, quote_chars="'\""):
+        self.ins = instream
+        self.outs = outstream
+        self.quote_chars = quote_chars
+
+    def minify(self, instream=None, outstream=None):
+        if instream and outstream:
+            self.ins, self.outs = instream, outstream
+        self.is_return = False
+        self.return_buf = ''
+        
+        def write(char):
+            if char in 'return':
+                self.return_buf += char
+                self.is_return = self.return_buf == 'return'
+            else:
+                self.return_buf = ''
+                self.is_return = self.is_return and char < '!'
+            self.outs.write(char)
+            if self.is_return:
+                self.return_buf = ''
+        read = self.ins.read
+        space_strings = "abcdefghijklmnopqrstuvwxyz"\
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$\\"
+        self.space_strings = space_strings
+        starters, enders = '{[(+-', '}])+-/' + self.quote_chars
+        newlinestart_strings = starters + space_strings + self.quote_chars
+        newlineend_strings = enders + space_strings + self.quote_chars
+        self.newlinestart_strings = newlinestart_strings
+        self.newlineend_strings = newlineend_strings
+        do_newline = False
+        do_space = False
+        escape_slash_count = 0
+        in_quote = ''
+        quote_buf = []
+        previous = ';'
+        previous_non_space = ';'
+        next1 = read(1)
+        while next1:
+            next2 = read(1)
+            if in_quote:
+                quote_buf.append(next1)
+
+                if next1 == in_quote:
+                    numslashes = 0
+                    for c in reversed(quote_buf[:-1]):
+                        if c != '\\':
+                            break
+                        else:
+                            numslashes += 1
+                    if numslashes % 2 == 0:
+                        in_quote = ''
+                        write(''.join(quote_buf))
+            elif next1 in '\r\n':
+                next2, do_newline = self.newline(
+                    previous_non_space, next2, do_newline)
+            elif next1 < '!':
+                if (previous_non_space in space_strings \
+                    or previous_non_space > '~') \
+                    and (next2 in space_strings or next2 > '~'):
+                    do_space = True
+                elif previous_non_space in '-+' and next2 == previous_non_space:
+                    do_space = True
+                elif self.is_return and next2 == '/':
+                    write(' ')
+            elif next1 == '/':
+                if do_space:
+                    write(' ')
+                if next2 == '/':
+                    next2 = self.line_comment(next1, next2)
+                    next1 = '\n'
+                    next2, do_newline = self.newline(
+                        previous_non_space, next2, do_newline)
+                elif next2 == '*':
+                    self.block_comment(next1, next2)
+                    next2 = read(1)
+                    if previous_non_space in space_strings:
+                        do_space = True
+                    next1 = previous
+                else:
+                    if previous_non_space in '{(,=:[?!&|;' or self.is_return:
+                        self.regex_literal(next1, next2)
+                        next2 = read(1)
+                    else:
+                        write('/')
+            else:
+                if do_newline:
+                    write('\n')
+                    do_newline = False
+                    do_space = False
+                if do_space:
+                    do_space = False
+                    write(' ')
+                write(next1)
+                if next1 in self.quote_chars:
+                    in_quote = next1
+                    quote_buf = []
+            if next1 >= '!':
+                previous_non_space = next1
+            if next1 == '\\':
+                escape_slash_count += 1
+            else:
+                escape_slash_count = 0
+            previous = next1
+            next1 = next2
+
+    def regex_literal(self, next1, next2):
+        assert next1 == '/'
+        self.return_buf = ''
+        read = self.ins.read
+        write = self.outs.write
+        in_char_class = False
+        write('/')
+        next = next2
+        while next and (next != '/' or in_char_class):
+            write(next)
+            if next == '\\':
+                write(read(1))
+            elif next == '[':
+                write(read(1))
+                in_char_class = True
+            elif next == ']':
+                in_char_class = False
+            next = read(1)
+        write('/')
+
+    def line_comment(self, next1, next2):
+        assert next1 == next2 == '/'
+        read = self.ins.read
+        while next1 and next1 not in '\r\n':
+            next1 = read(1)
+        while next1 and next1 in '\r\n':
+            next1 = read(1)
+        return next1
+
+    def block_comment(self, next1, next2):
+        assert next1 == '/'
+        assert next2 == '*'
+        read = self.ins.read
+        next1 = read(1)
+        next2 = read(1)
+        comment_buffer = '/*'
+        while next1 != '*' or next2 != '/':
+            comment_buffer += next1
+            next1 = next2
+            next2 = read(1)
+
+        if comment_buffer.startswith("/*!"):
+            self.outs.write(comment_buffer)
+            self.outs.write("*/\n")
+
+    def newline(self, previous_non_space, next2, do_newline):
+        read = self.ins.read
+        if previous_non_space and (
+                        previous_non_space in self.newlineend_strings
+                        or previous_non_space > '~'):
+            while 1:
+                if next2 < '!':
+                    next2 = read(1)
+                    if not next2:
+                        break
+                else:
+                    if next2 in self.newlinestart_strings \
+                            or next2 > '~' or next2 == '/':
+                        do_newline = True
+                    break
+        return next2, do_newline
+
+def list_js_files():
+    js_files = [f for f in os.listdir() if f.endswith('.js')]
+    if not js_files:
+        print("No JavaScript files found in the current directory.")
+    return js_files
+
+def select_js_file(js_files):
+    print("Select a JavaScript file to minify:")
+    for idx, file in enumerate(js_files, start=1):
+        print(f"{idx}. {file}")
+    choice = int(input("Enter the number corresponding to the file: ")) - 1
+    return js_files[choice]
+
+def minify_js_file(file_path):
+    with open(file_path, 'r') as file:
+        minified_code = jsminifier(file.read())
+    minified_file_path = f"minified_{file_path}"
+    with open(minified_file_path, 'w') as minified_file:
+        minified_file.write(minified_code)
+    print(f"Minified file saved as: {minified_file_path}")
+
+def minifyjs():
+    js_files = list_js_files()
+    if js_files:
+        selected_file = select_js_file(js_files)
+        minify_js_file(selected_file)
+
 def __init__():
     parser = argparse.ArgumentParser(description="rjj will execute different functions based on command-line arguments")
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
@@ -3140,6 +3343,7 @@ def __init__():
     subparsers.add_parser('donut', help='bake a donut')
     subparsers.add_parser('output', help='output as csv and json')
     subparsers.add_parser('report', help='generate report')
+    subparsers.add_parser('minify', help='minify js')
     subparsers.add_parser('code', help='encode or decode')
     subparsers.add_parser('join', help='join it up')
     subparsers.add_parser('home', help='go home')
@@ -3184,6 +3388,8 @@ def __init__():
         joint()
     elif args.subcommand == 'code':
         base64codeHandler()
+    elif args.subcommand == 'minify':
+        minifyjs()
     elif args.subcommand == 'report':
         generate_reports()
     elif args.subcommand == 'output':
